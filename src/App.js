882cd1e7e3fa20302c7f39bb1ad9654d51a8f966
lingqingran <1767525738@qq.com>
2022-07-11 21:34:36 +0800

    user places

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import Users from './user/pages/Users'
 import NewPlace from './places/pages/NewPlace'
+import UserPlaces from './places/pages/UserPlaces'
 import MainNavigation from './shared/components/Navigation/MainNavigation'
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
       <main>
         <Routes>
           <Route path="/" element={<Users />} />
+          <Route path="/:userId/places" element={<UserPlaces />} />
           <Route path="/places/new" element={<NewPlace />} />
         </Routes>
       </main>
